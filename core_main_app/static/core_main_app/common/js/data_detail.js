1eb55541abc21835fd30f5e07eba1ddb886617d9
MA042024 <mohammad.abbas@empa.ch>
2024-05-15 14:54:49 +0200

    Load to GV Form
    Add Load to GV Form Functionality

--- core_main_app/static/core_main_app/common/js/data_detail.js
+++ core_main_app/static/core_main_app/common/js/data_detail.js
@@ -7,3 +7,32 @@ var downloadOptions = function() {
 
 
 $(document).on('click', '.btn.download', downloadOptions);
+
+
+
+var loadXmlData = function(event) {
+    event.preventDefault();
+    $.ajax({
+        url: loadDocumentUrl,
+        type: 'POST',
+        contentType: 'application/json',
+        success: function(response) {
+            var data_id = response.data_id;
+            var data_content = response.data_content;
+            var data_title = response.data_title;
+            var test_id = response.test_id;
+            sessionStorage.setItem('data_content', JSON.stringify(data_content));
+            sessionStorage.setItem('data_title', data_title); 
+            sessionStorage.setItem('test_id', test_id); 
+            window.location.href = '/gensel';
+        },
+        error: function(xhr, status, error) {
+            console.error('Failed to load data:', error);
+            console.error('XHR:', xhr);
+            console.error('Status:', status);
+            console.error('Error:', error); 
+        }
+    });
+};
+
+$(document).on('click', '.load-btn', loadXmlData);
