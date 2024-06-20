fd27818ed3ff5612450614c94fbdcb555ac2121b
MA042024 <mohammad.abbas@empa.ch>
2024-06-20 11:50:42 +0200

    Update data_detail.js

--- core_main_app/static/core_main_app/common/js/data_detail.js
+++ core_main_app/static/core_main_app/common/js/data_detail.js
@@ -10,18 +10,7 @@ $(document).on('click', '.btn.download', downloadOptions);
 
 
 var loadXmlData = function() {
-    $.ajax({
-        url: loadDocumentUrl,  // URL is set from the other JS variable
-        type: 'POST',
-        data: { 'data_id': data.data.id },
-        success: function(response) {
-            console.log('Data sent successfully');
-            console.log(response.message);
-        },
-        error: function(xhr, status, error) {
-            console.error('Error sending data:', error);
-        }
-    });
+    window.location.href = loadDocumentUrl;
 };
 
 $(document).on('click', '.load-btn', loadXmlData);
