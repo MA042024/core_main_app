9b82bbdf18ad2e7f28033aafb1bc6da5605a0d1b
MA042024 <mohammad.abbas@empa.ch>
2024-06-20 13:28:54 +0200

    Update data_detail.js

--- core_main_app/static/core_main_app/common/js/data_detail.js
+++ core_main_app/static/core_main_app/common/js/data_detail.js
@@ -9,8 +9,23 @@ var downloadOptions = function() {
 $(document).on('click', '.btn.download', downloadOptions);
 
 
-var loadXmlData = function() {
-    window.location.href = loadDocumentUrl;
+var loadXmlData = function(event) {
+    //event.preventDefault();  // Prevent the default action of the event (e.g., navigating away)
+    
+    // Your data loading logic here
+    $.ajax({
+        url: loadDocumentUrl,
+        type: 'POST',  // Adjust the type as needed
+        data: {},  // Optional data to send with the request
+        success: function(response) {
+            console.log('Data loaded successfully:', response);
+            // Handle success (update UI, display message, etc.)
+        },
+        error: function(xhr, status, error) {
+            console.error('Failed to load data:', error);
+            // Handle error (display error message, etc.)
+        }
+    });
 };
 
 $(document).on('click', '.load-btn', loadXmlData);
