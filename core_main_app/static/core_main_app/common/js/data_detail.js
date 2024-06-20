5b0eec6033a67a86e7b33b9e0191256a313744b2
MA042024 <mohammad.abbas@empa.ch>
2024-06-20 11:40:44 +0200

    Update data_detail.js

--- core_main_app/static/core_main_app/common/js/data_detail.js
+++ core_main_app/static/core_main_app/common/js/data_detail.js
@@ -9,37 +9,19 @@ var downloadOptions = function() {
 $(document).on('click', '.btn.download', downloadOptions);
 
 
-
 var loadXmlData = function() {
     $.ajax({
-        url: '/data/load/',
+        url: loadDocumentUrl,  // URL is set from the other JS variable
         type: 'POST',
         data: { 'data_id': data.data.id },
         success: function(response) {
-            console.log('XML data sent successfully');
-            console.log(response.data_id);
-            console.log(response.data_content);
-            // Send the data to /gensel/
-            /*$.ajax({
-                url: '/gensel/',
-                type: 'POST',
-                data: {
-                    'data_id': response.data_id,
-                    'data_content': response.data_content
-                },
-                success: function() {
-                    console.log('Data successfully sent to /gensel/');
-                },
-                error: function(xhr, status, error) {
-                    console.error('Error sending data to /gensel/:', error);
-                }
-            });*/
+            console.log('Data sent successfully');
+            console.log(response.message);
         },
         error: function(xhr, status, error) {
-            console.error('Error sending XML data:', error);
+            console.error('Error sending data:', error);
         }
     });
 };
 
 $(document).on('click', '.load-btn', loadXmlData);
-
