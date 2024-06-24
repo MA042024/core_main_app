76e1eb06e61c5d8217c3ecd3651407b1d849aa6b
MA042024 <mohammad.abbas@empa.ch>
2024-06-24 12:37:55 +0200

    Update data_detail.js

--- core_main_app/static/core_main_app/common/js/data_detail.js
+++ core_main_app/static/core_main_app/common/js/data_detail.js
@@ -13,27 +13,25 @@ $(document).on('click', '.btn.download', downloadOptions);
 console.log('CSRF Token:', csrftoken);  // Debugging: Print the CSRF token
 
 var loadXmlData = function(event) {
-    event.preventDefault();  // Prevent the default action of the event (e.g., navigating away)
+    event.preventDefault();
     
-    console.log('loadDocumentUrl:', loadDocumentUrl);  // Debugging: Print the URL
+    console.log('CSRF Token:', csrftoken);
+    console.log('loadDocumentUrl:', loadDocumentUrl);
 
-    // Your data loading logic here
     $.ajax({
         url: loadDocumentUrl,
-        type: 'POST',  // Adjust the type as needed
-        headers: {
+        type: 'POST',
+       /* headers: {
             'X-CSRFToken': csrftoken
-        },
+        },*/
         success: function(response) {
             console.log('Data loaded successfully:', response);
-            // Handle success (update UI, display message, etc.)
         },
         error: function(xhr, status, error) {
             console.error('Failed to load data:', error);
-            console.error('XHR:', xhr);  // Debugging: Print the XHR object
-            console.error('Status:', status);  // Debugging: Print the status
-            console.error('Error:', error);  // Debugging: Print the error
-            // Handle error (display error message, etc.)
+            console.error('XHR:', xhr);
+            console.error('Status:', status);
+            console.error('Error:', error); 
         }
     });
 };
