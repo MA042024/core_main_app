92bdbe95a1928252711a5952f469f271541f3cb9
MA042024 <mohammad.abbas@empa.ch>
2024-07-09 11:01:12 +0200

    Update data_detail.js

--- core_main_app/static/core_main_app/common/js/data_detail.js
+++ core_main_app/static/core_main_app/common/js/data_detail.js
@@ -10,35 +10,18 @@ $(document).on('click', '.btn.download', downloadOptions);
 
 
 
-console.log('CSRF Token:', csrftoken);  // Debugging: Print the CSRF token
-
 var loadXmlData = function(event) {
     event.preventDefault();
-    
-    console.log('CSRF Token:', csrftoken);
-    console.log('loadDocumentUrl:', loadDocumentUrl);
-
     $.ajax({
         url: loadDocumentUrl,
         type: 'POST',
-       /* headers: {
-            'X-CSRFToken': csrftoken
-        },*/
         contentType: 'application/json',
         success: function(response) {
-            console.log('Data loaded successfully');
             var data_id = response.data_id;
             var data_content = response.data_content;
             var data_title = response.data_title;
-            
-            console.log('Data ID:', data_id);
-            console.log('Data Content:', data_content);
-            console.log('Data Title:', data_title);
-
-            /*localStorage.setItem('data_id', data_id);*/
             localStorage.setItem('data_content', JSON.stringify(data_content));
             localStorage.setItem('data_title', data_title);
-
             window.location.href = '/gensel';
         },
         error: function(xhr, status, error) {
